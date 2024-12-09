2133a3d36a7b2dbb1bd354078ef3fd550115c8e6
Datskova <denitsa.datskova@gmail.com>
2024-12-09 13:30:31 +0200

    add storage in main

--- main.tf
+++ main.tf
@@ -5,6 +5,12 @@ terraform {
       version = "4.13.0"
     }
   }
+    backend "azurerm" {
+      resource_group_name = "StorageRG"
+      storage_account_name = "taskboardstoragedeni"
+      container_name = "taskboardstoragedeni"
+      key = "terraform.tfstate" 
+  }
 }
 
 provider "azurerm" {
